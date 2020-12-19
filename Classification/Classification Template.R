af87198fbe764faed3d22bd86f4358723c631f4a
JS <jsvmelon@gmail.com>
2020-12-19 17:13:30 +0000

    Improved the template by using a function for the visualisation

--- Classification/Classification Template.R
+++ Classification/Classification Template.R
@@ -25,39 +25,26 @@ y_pred <- predict(classifier, newdata = test_set[-3])
 cm <- table(test_set[, 3], y_pred)
 
 # visualise the results for the training set
-library(ElemStatLearn)
-set <- training_set
-X1 <- seq(min(set[,1]) - 1, max(set[,1]) + 1, by = 0.01)
-X2 <- seq(min(set[,2]) - 1, max(set[,2]) + 1, by = 0.01)
-grid_set <- expand.grid(X1,X2)
-colnames(grid_set) = c("Age","EstimatedSalary")
-y_grid <- y_pred <- predict(classifier, newdata = grid_set[-3])
+display(training_set,"XYZ (Training Set")
+display(test_set,"XYZ (Test Set")
 
-plot(set[, -3],
-     main = "logistic regresssion (training set)",
-     xlab = "Age" , ylab = "Estimated Salary",
-     xlim = range(X1), ylim = range(X2))
-
-contour(X1,X2,matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = ".", col = ifelse(y_grid == 1, "springgreen3", "tomato"))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, "green4", "red3"))
-
-# visualise the results for the test set
-library(ElemStatLearn)
-set <- test_set
-X1 <- seq(min(set[,1]) - 1, max(set[,1]) + 1, by = 0.01)
-X2 <- seq(min(set[,2]) - 1, max(set[,2]) + 1, by = 0.01)
-grid_set <- expand.grid(X1,X2)
-colnames(grid_set) = c("Age","EstimatedSalary")
-y_grid <- y_pred <- predict(classifier, newdata = grid_set[-3])
-plot(set[, -3],
-     main = "logistic regresssion (Test set)",
-     xlab = "Age" , ylab = "Estimated Salary",
-     xlim = range(X1), ylim = range(X2))
-
-contour(X1,X2,matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
-points(grid_set, pch = ".", col = ifelse(y_grid == 1, "springgreen3", "tomato"))
-points(set, pch = 21, bg = ifelse(set[, 3] == 1, "green4", "red3"))
+display <- function(set,title) {
+        library(ElemStatLearn)
+        X1 <- seq(min(set[,1]) - 1, max(set[,1]) + 1, by = 0.02)
+        X2 <- seq(min(set[,2]) - 1, max(set[,2]) + 1, by = 0.02)
+        grid_set <- expand.grid(X1,X2)
+        colnames(grid_set) = c("Age","EstimatedSalary")
+        y_grid <- predict(classifier, newdata = grid_set)
+        
+        plot(set[, -3],
+             main = title,
+             xlab = "Age" , ylab = "Estimated Salary",
+             xlim = range(X1), ylim = range(X2))
+        
+        contour(X1,X2,matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+        points(grid_set, pch = ".", col = ifelse(y_grid == 1, "springgreen3", "tomato"))
+        points(set, pch = 21, bg = ifelse(set[, 3] == 1, "green4", "red3"))
+}
 
 
 
