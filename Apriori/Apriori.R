1b16be73fbbc36065feb39cea68f871f0f917a8a
JS <jsvmelon@gmail.com>
2021-01-05 22:19:25 +0000

    Apriori association rule exercise

--- Apriori/Apriori.R
+++ Apriori/Apriori.R
@@ -1,5 +1,16 @@
-# Apriori
+# Apriori Association Rule
 
-# data preprocessing
-dataset <- read.csv("Market_Basket_Optimisation.csv", header = FALSE)
+#dataset <- read.csv("Market_Basket_Optimisation.csv", header = FALSE)
 
+library(arules)
+dataset <- read.transactions("Market_Basket_Optimisation.csv", sep = ",", rm.duplicates = TRUE)
+
+summary(dataset)
+itemFrequencyPlot(dataset, topN = 10)
+
+# training apriori on the dataset
+parameter <- list(support = 0.004, confidence = 0.2)
+rules <- apriori(data = dataset, parameter = parameter)
+
+# visualising the results
+inspect(sort(rules, by = "lift")[1:10])
